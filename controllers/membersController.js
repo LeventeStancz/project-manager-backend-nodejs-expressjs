daa057fe0ed3e5a7cf6c6fad53c101f5a119bb1c
LeventeStancz <stancz.levente0304@gmail.com>
2023-08-13 00:02:54 +0200

    feat: remove member from project

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -188,7 +188,102 @@ const addMember = async (req, res) => {
   }
 };
 
+const removeMember = async (req, res) => {
+  const { projectname, memberid } = req.params;
+
+  if (
+    typeof projectname === "undefined" ||
+    !mongoose.Types.ObjectId.isValid(req.user) ||
+    !mongoose.Types.ObjectId.isValid(memberid)
+  ) {
+    return res.status(400).json({
+      clientMsg: "Not enough information.",
+      error:
+        "No projectid/memberud in the request body when trying to remove member from project.",
+    });
+  }
+
+  try {
+    const { isAdmin } = await User.findOne(
+      { _id: req.user },
+      { isAdmin: 1, _id: 0 }
+    ).exec();
+
+    //get the project id for easier searches
+    const { _id: projectId } = await Project.findOne({
+      name: projectname,
+    }).exec();
+
+    if (!isAdmin) {
+      //check if owner if not an admin
+      const { owner: curOwner, isActive } = await Project.findOne(
+        { _id: projectId },
+        { owner: 1, isActive: 1, _id: 0 }
+      ).exec();
+
+      if (req.user.toString() !== curOwner.toString()) {
+        return res.status(401).json({
+          clientMsg:
+            "You don't have authority to remove a member from this project.",
+          error: "User is not the owner of the project.",
+        });
+      }
+
+      //check if project is inactive
+      if (!isActive) {
+        return res.status(401).json({
+          clientMsg: "This project is inactive.",
+          error:
+            "The project the user is trying to remove member from is inactive.",
+        });
+      }
+    }
+
+    //check if member is the owner
+    const { owner: curOwner } = await Project.findOne(
+      { _id: projectId },
+      { owner: 1, _id: 0 }
+    ).exec();
+
+    if (memberid.toString() === curOwner.toString()) {
+      return res.status(401).json({
+        clientMsg: "You can't remove the project owner.",
+        error: "Member is the owner of the project.",
+      });
+    }
+
+    //check if member is truly a member
+    const member = await ProjectMember.findOne({
+      project: projectId,
+      user: memberid,
+    });
+
+    if (!member) {
+      return res.status(401).json({
+        clientMsg: "This user is not a member of this project.",
+        error: "The user getting removed is not member of this project.",
+      });
+    }
+
+    await ProjectMember.deleteOne({
+      project: projectId,
+      user: memberid,
+    });
+
+    return res.status(200).json({
+      clientMsg: "Successfully removed member from project!",
+      error: "",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      clientMsg: "Something went wrong. Try again later!",
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
   getMembersInProject,
   addMember,
+  removeMember,
 };
