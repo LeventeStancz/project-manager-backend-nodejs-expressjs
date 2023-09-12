f13f9124c3a8f2d8151e3818821930b58c74a142
LeventeStancz <stancz.levente0304@gmail.com>
2023-09-12 22:27:09 +0200

    fix: allow origin by cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,13 @@
 //remove/add some at build
 const allowedOrigins = [
-  "https://technotes.onrender.com",
-  "https://technotes.onrender.com:3000",
-  "https://project-manager-nuxi.onrender.com",
-  "https://project-manager-nuxi.onrender.com:3000",
+  "https://technotes.onrender.com/",
+  "http://technotes.onrender.com/",
+  "https://technotes.onrender.com:3000/",
+  "http://technotes.onrender.com:3000/",
+  "https://project-manager-nuxi.onrender.com/",
+  "http://project-manager-nuxi.onrender.com/",
+  "https://project-manager-nuxi.onrender.com:3000/",
+  "http://project-manager-nuxi.onrender.com:3000/",
 ];
 
 module.exports = allowedOrigins;
