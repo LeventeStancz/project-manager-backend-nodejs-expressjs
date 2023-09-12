bfa9db857a801be4b36b426d4d6fb0bff9a3ee22
LeventeStancz <stancz.levente0304@gmail.com>
2023-09-12 22:52:06 +0200

    fix: allow origin by cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,13 +1,6 @@
 //remove/add some at build
 const allowedOrigins = [
-  "https://technotes.onrender.com/",
-  "http://technotes.onrender.com/",
-  "https://technotes.onrender.com:3000/",
-  "http://technotes.onrender.com:3000/",
-  "https://project-manager-nuxi.onrender.com/",
-  "http://project-manager-nuxi.onrender.com/",
-  "https://project-manager-nuxi.onrender.com:3000/",
-  "http://project-manager-nuxi.onrender.com:3000/",
+  "https://project-manager-nuxi.onrender.com",
 ];
 
 module.exports = allowedOrigins;
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,8 +2,7 @@ const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (origin) console.log(origin);
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       //error: null, sameOrigin: true
       callback(null, true);
     } else {
