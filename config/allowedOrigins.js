7f22d9a18ffb05a177856bd93e528a84a7484549
LeventeStancz <stancz.levente0304@gmail.com>
2023-09-12 20:29:29 +0200

    fix: allow origin by cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,8 +2,6 @@
 const allowedOrigins = [
   "https://project-manager-nuxi.onrender.com",
   "https://project-manager-nuxi.onrender.com:3000",
-  "https://project-manager-api-xo8j.onrender.com",
-  "https://project-manager-api-xo8j.onrender.com:5000",
 ];
 
 module.exports = allowedOrigins;
