f98f6518e75e7853157f28e0521131aba810b53f
LeventeStancz <stancz.levente0304@gmail.com>
2023-09-12 20:49:30 +0200

    fix: allow oirigin by cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 //remove/add some at build
 const allowedOrigins = [
+  "https://technotes.onrender.com",
+  "https://technotes.onrender.com:3000",
   "https://project-manager-nuxi.onrender.com",
   "https://project-manager-nuxi.onrender.com:3000",
 ];
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,9 +1,7 @@
 const allowedOrigins = require("./allowedOrigins");
-const { logEvents } = require("../middlewares/logEvents");
 
 const corsOptions = {
   origin: (origin, callback) => {
-    logEvents(`${origin}`, "requestLog.txt");
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       //error: null, sameOrigin: true
       callback(null, true);
